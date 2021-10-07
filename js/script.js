3fb1639bfde6728d354e4c30d741390633db6ce3
RevanthR <revanth.rallabandi@gmail.com>
2021-10-08 03:27:21 +0530

    Map Added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 20.59 , lng: 78.9 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+}
+//Google Maps Script Above
