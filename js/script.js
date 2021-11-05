2edd398ad2d66a25c4bc4694b82ebfe306bb3935
Rallabandi, Revanth <revanth.rallabandi@geappliances.com>
2021-11-06 00:36:53 +0530

    Only MediaQueries Left

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function initMap() {
-  var center = {lat: 24.5, lng: 78.01};
+  var center = { lat: 24.5, lng: 78.01 };
   var locations = [
     ['SVS Refcomp Pvt Limited<br>\
     Q86J+X8F<br>\
     Maharashtra 412210<br>\
-   <a href="https://goo.gl/maps/M5pdCDzTYQp3rVoF6">Get Directions</a>',   18.762665778713465, 74.33082795501],
+   <a href="https://goo.gl/maps/M5pdCDzTYQp3rVoF6">Get Directions</a>', 18.762665778713465, 74.33082795501],
 
     ['SVS Refcomp<br>\
       Hyderbad, Narsapur Rd, Telangana<br>\
@@ -16,7 +16,7 @@ function initMap() {
 
     ['SVS Wires Pvt Ltd.<br>\
       Plot No. D-37, Phase IV Extension, Jeedimetla, Hyderabad, Telangana 500055<br>\
-    <a href="https://goo.gl/maps/qLUWqSVT4jGpAS7c8">Get Directions</a>',17.521910800205536, 78.44033726845059],
+    <a href="https://goo.gl/maps/qLUWqSVT4jGpAS7c8">Get Directions</a>', 17.521910800205536, 78.44033726845059],
 
     ['SVS Novatech PVT LTD<br>\
       Ahmedabad, Gujarat 382110<br>\
@@ -26,23 +26,23 @@ function initMap() {
       Sikandrabad, Bulandshahr, Uttar Pradesh 203206<br>\
    <a href="https://goo.gl/maps/MP1TZpDePgykHdEq8">Get Directions</a>', 28.46548456102284, 77.66452509748649],
 
-   ['SVS Wires Pvt. Ltd<br>\
+    ['SVS Wires Pvt. Ltd<br>\
       Talabpur Urf hathipur, Uttar Pradesh 203207<br>\
    <a href="https://goo.gl/maps/LJPRNVYn3XpKvUus6">Get Directions</a>', 28.618278856779103, 77.49293100493945]
   ];
-var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 5,
     center: center
   });
-var infowindow =  new google.maps.InfoWindow({});
-var marker, count;
-for (count = 0; count < locations.length; count++) {
+  var infowindow = new google.maps.InfoWindow({});
+  var marker, count;
+  for (count = 0; count < locations.length; count++) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[count][1], locations[count][2]),
       map: map,
       title: locations[count][0]
     });
-google.maps.event.addListener(marker, 'click', (function (marker, count) {
+    google.maps.event.addListener(marker, 'click', (function (marker, count) {
       return function () {
         infowindow.setContent(locations[count][0]);
         infowindow.open(map, marker);
@@ -50,3 +50,36 @@ google.maps.event.addListener(marker, 'click', (function (marker, count) {
     })(marker, count));
   }
 };
+
+// Code to switch betweem tabs
+
+$(document).ready(function () {
+  //grabs the hash tag from the url
+  var hash = window.location.hash;
+  //checks whether or not the hash tag is set
+  if (hash != "") {
+    //removes all active classes from tabs
+    $('#nav-tab button').each(function () {
+      $(this).removeClass('active');
+    });
+    $('#nav-tabContent div').each(function () {
+      $(this).removeClass('show active');
+    });
+    //this will add the active class on the hashtagged value
+    var link = "";
+    $('#nav-tab button').each(function () {
+      link = $(this).attr('data-bs-target');
+      if (link == hash) {
+        $(this).addClass('active');
+      }
+    });
+    $('#nav-tabContent div').each(function () {
+
+      link = $(this).attr('id');
+      if ('#' + link == hash) {
+
+        $(this).addClass('show active');
+      }
+    });
+  }
+});
